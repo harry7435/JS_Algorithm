cd5ed5a1328e9393627e5a53d25b8a7f6701302f
Cho_Ik_Jun <harry960629@naver.com>
2023-09-08 23:35:24 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -4,12 +4,13 @@ function solution(cards1, cards2, goal) {
     let card2 = '';
    
     for (let i=0; i<goal.length; i++) {
+        // 카드1 체크
         card1 = cards1.shift();
-        if (goal[i] === card1) continue;
+        if (goal[i] === card1) continue; // 같으면 넘어가기
         cards1.unshift(card1);
-        
+        // 카드2 체크
         card2 = cards2.shift();
-        if (goal[i] === card2) continue;
+        if (goal[i] === card2) continue; // 같으면 넘어가기
         cards2.unshift(card2);
         answer = 'No';
     }
