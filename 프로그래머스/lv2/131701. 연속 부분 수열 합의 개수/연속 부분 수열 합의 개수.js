c3430361a89ddd1dade4a5dd5bde864f895a480e
Cho_Ik_Jun <harry960629@naver.com>
2023-09-12 01:22:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1529.23 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,14 +1,13 @@
 function solution(elements) {
     var answer = 0;
-    
-    let circle = [...elements];
-    
+    let circle = [...elements]; 
+    // 원형 수열 만들기 => 두 번 이어붙이기
     for (let i=0; i<elements.length; i++) {
         circle.push(elements[i]);
     }
         
-    let sumSet = new Set();
-    
+    let sumSet = new Set(); // 부분 수열 합 집합
+    // 부분 수열 체크
     for (let start=0; start<elements.length; start++) {
         for (let n=1; n<elements.length+1; n++) {
             let end = start+n;
@@ -16,7 +15,7 @@ function solution(elements) {
             circle.slice(start, end).forEach((number) => {
                 tempSum += number;
             });
-            
+            // 집합에 요소가 없으면 추가
             if (!sumSet.has(tempSum)) {
                 sumSet.add(tempSum);
             }
@@ -24,7 +23,7 @@ function solution(elements) {
         }
     }
     
-    answer = sumSet.size;
+    answer = sumSet.size; // 집합 요소 갯수 반환
     
     return answer;
 }
