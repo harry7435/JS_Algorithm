976bc59be55d246ba163f7481af8a802a7630b1c
Cho_Ik_Jun <harry960629@naver.com>
2023-09-23 20:25:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,11 +1,12 @@
 function solution(n)
 {
-    let ans = 0;
- 
+    let ans = 0; // 최소 배터리 소진량
+    // 역으로 2로 나누어 떨어지지 않는 경우를 체크
     while (n > 0) {
+        // 2로 나누어지면 순간이동 가능 + 1
         if (n % 2 === 0) {
             n = parseInt(n / 2);
-        } else {
+        } else { // 배터리 소진 + 1
             ans += 1;
             n -= 1;
         }
